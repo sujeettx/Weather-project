4bc07f6a518b40749c446731dfdcc9819a7ae4ba
sujeettx <sujeetkushwaha0123@gmail.com>
2025-01-30 10:03:47 +0530

    Stop tracking config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Config
-const CONFIG = {
-    API_KEY: '2a076f927ed74dfa9a794530240108',
-    BASE_URL: 'https://api.weatherapi.com/v1',
-    DEFAULT_LOCATION: 'London'
-  };
-  
-  module.exports = CONFIG;
